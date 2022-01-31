5365cf1d09f84aa1be877b7b78c94a72740975d7
Emanueljuarez <zerauleuname@gmail.com>
2022-01-31 11:19:58 -0600

    Servidor creado

---
+++ index.js
@@ -0,0 +1,43 @@
+// ./index.js
+
+/**
+ * PATRONES DE DISEÑO
+ * SON PROPUESTAS Y CASOS DE USO QUE PERMITEN RESOLVER PROBLEMAS COTIDIANOS
+ * 
+ * 1. MODULE PATTERN - PATRÓN MODULAR
+ * 
+ * APLICA VALIDEZ DE IMPORTACIONES Y EXPORTACIONES EN UN PROYECTO
+ * 
+ */
+
+
+// 1. IMPORTACIONES
+// PROCESO DE TRAERNOS FUNCIONES O VARIABLES DE OTROS ARCHIVOS JS A ESTE
+
+//A. ARCHIVOS EXTERNOS PROPIOS
+// const suma = require("./suma")
+// console.log(suma(2,4))
+
+// const resta = require("./resta")
+// console.log(resta(6,2))
+
+///B. ARCHIVOS DE NODE-MODULES
+const express   = require("express")
+const app       = express()
+
+
+
+
+// 2. MIDDLEWARES
+
+
+
+// 3. RUTAS
+app.use("/", require("./routes/index"))
+
+
+// 4. SERVIDOR
+
+app.listen(3005, () => {
+	console.log("Servidor activo")
+})
---
+++ suma.js
@@ -0,0 +1,11 @@
+// ./suma.js
+
+const suma = (a, b) => {
+
+	return a + b
+
+}
+
+// EXPORTACIÓN
+// PERMITIR DISPONIBILIDAD AL PÚBLICO
+module.exports = suma
